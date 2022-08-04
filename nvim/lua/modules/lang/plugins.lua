97a76762938c4c96bfbc9589efdaf7fb0b8fc7c5
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-08-04 13:54:11 +0800

    docs: Finishing lang setting

--- nvim/lua/modules/lang/plugins.lua
+++ nvim/lua/modules/lang/plugins.lua
@@ -1,28 +1,14 @@
 local lang = {}
 local conf = require("modules.lang.config")
 
-lang["fatih/vim-go"] = {
-	opt = true,
-	ft = "go",
-	run = ":GoInstallBinaries",
-	config = conf.lang_go,
-}
-lang["rust-lang/rust.vim"] = { opt = true, ft = "rust" }
-lang["simrat39/rust-tools.nvim"] = {
-	opt = true,
-	ft = "rust",
-	config = conf.rust_tools,
-	requires = { { "nvim-lua/plenary.nvim", opt = false } },
-}
--- lang["kristijanhusak/orgmode.nvim"] = {
---     opt = true,
---     ft = "org",
---     config = conf.lang_org
--- }
+-- Markdown-preview out of box
 lang["iamcco/markdown-preview.nvim"] = {
 	opt = true,
 	ft = "markdown",
 	run = "cd app && yarn install",
 }
+
+-- Plugin for csv
 lang["chrisbra/csv.vim"] = { opt = true, ft = "csv" }
+
 return lang
